444e4dfb5c28295553bee520681a0c687e546d29
Taryn Dunn <tarynad@iastate.edu>
2025-09-08 19:34:44 -0500

    changed exp2, simple sign up using Intel

--- Experiments/Taryn/exp2/tutorials-android_unit1_2_intent/AndroidExample/app/src/main/java/com/example/androidexample/CounterActivity.java
+++ Experiments/Taryn/exp2/tutorials-android_unit1_2_intent/AndroidExample/app/src/main/java/com/example/androidexample/CounterActivity.java
@@ -10,12 +10,9 @@ import android.widget.TextView;
 
 public class CounterActivity extends AppCompatActivity {
 
-    private TextView numberTxt; // define number textview variable
-    private Button increaseBtn; // define increase button variable
-    private Button decreaseBtn; // define decrease button variable
+    private TextView counterMsg;
     private Button backBtn;     // define back button variable
 
-    private int counter = 0;    // counter variable
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,33 +20,21 @@ public class CounterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_counter);
 
         /* initialize UI elements */
-        numberTxt = findViewById(R.id.number);
-        increaseBtn = findViewById(R.id.counter_increase_btn);
-        decreaseBtn = findViewById(R.id.counter_decrease_btn);
+        counterMsg = findViewById(R.id.counter_text);
         backBtn = findViewById(R.id.counter_back_btn);
 
-        /* when increase btn is pressed, counter++, reset number textview */
-        increaseBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                numberTxt.setText(String.valueOf(++counter));
-            }
-        });
 
-        /* when decrease btn is pressed, counter--, reset number textview */
-        decreaseBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                numberTxt.setText(String.valueOf(--counter));
-            }
-        });
+        Bundle extras = getIntent().getExtras();
+        if(extras != null) {
+            String userEmail = extras.getString("Email");
+            counterMsg.setText("Hello " + userEmail + "!");
+        }
 
         /* when back btn is pressed, switch back to MainActivity */
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(CounterActivity.this, MainActivity.class);
-                intent.putExtra("NUM", String.valueOf(counter));  // key-value to pass to the MainActivity
                 startActivity(intent);
             }
         });
--- Experiments/Taryn/exp2/tutorials-android_unit1_2_intent/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/Taryn/exp2/tutorials-android_unit1_2_intent/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -13,7 +14,8 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
 
     private TextView messageText;     // define message textview variable
-    private Button counterButton;     // define counter button variable
+    private Button counterButton;
+    private EditText emailBox;// define counter button variable
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,23 +25,22 @@ public class MainActivity extends AppCompatActivity {
         /* initialize UI elements */
         messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
         counterButton = findViewById(R.id.main_counter_btn);// link to counter button in the Main activity XML
+        emailBox = findViewById(R.id.main_email_box);
 
         /* extract data passed into this activity from another activity */
-        Bundle extras = getIntent().getExtras();
-        if(extras == null) {
-            messageText.setText("Intent Example");
-        } else {
-            String number = extras.getString("NUM");  // this will come from LoginActivity
-            messageText.setText("The number was " + number);
-        }
+
+            messageText.setText("Enter email to sign up");
+
 
         /* click listener on counter button pressed */
         counterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String userEmail = emailBox.getText().toString().trim();
 
                 /* when counter button is pressed, use intent to switch to Counter Activity */
                 Intent intent = new Intent(MainActivity.this, CounterActivity.class);
+                intent.putExtra("Email", userEmail);
                 startActivity(intent);
             }
         });
