0c17b4c8a1bc554b078a5260f8fbe83c4af77f9f
Sean Krueger <skrueger2270@gmail.com>
2025-09-10 10:02:25 -0500

    Exp3: Replace petclinic with helloworld

--- Experiments/Sean/Exp_3/src/main/java/coms309/WelcomeController.java
+++ Experiments/Sean/Exp_3/src/main/java/coms309/WelcomeController.java
@@ -1,14 +1,19 @@
-package org.springframework.samples.petclinic.system;
-
+package coms309;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Simple Hello World Controller to display the string returned
+ *
+ * @author Vivek Bengre
+ */
 
 @RestController
 class WelcomeController {
 
     @GetMapping("/")
     public String welcome() {
-        return "Welcome</br> Go to localhost:8080/owners/create to create dummy data </br>";
+        return "Hello and welcome to COMS 309";
     }
 }
