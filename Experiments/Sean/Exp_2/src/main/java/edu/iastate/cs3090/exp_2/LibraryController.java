fd46ffb59c68ec7a245fba5b49c9a0485b40051a
Sean Krueger <skrueger2270@gmail.com>
2025-09-11 03:19:20 -0500

    Add filtering and sorting function

--- Experiments/Sean/Exp_2/src/main/java/edu/iastate/cs3090/exp_2/LibraryController.java
+++ Experiments/Sean/Exp_2/src/main/java/edu/iastate/cs3090/exp_2/LibraryController.java
@@ -1,19 +1,73 @@
 package edu.iastate.cs3090.exp_2;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
 public class LibraryController {
     private final HashMap<Long, Book> books = new HashMap<Long, Book>();
 
-    // LIST
-    @GetMapping("/books")
-    public HashMap<Long, Book> getBooks() {
-        return books;
+    @GetMapping(value = "/books")
+    public ArrayList<Book> getBooks(@RequestParam(required = false) MultiValueMap<String, String> params) {
+        ArrayList<Book> filteredBooks = new ArrayList<>(books.values());
+        ArrayList<Book> tempFilteredBooks = new ArrayList<>();
+
+        if (params.isEmpty()) {
+            return filteredBooks;
+        }
+
+        if (params.containsKey("filter")) {
+            List<String> filterList = (List<String>) params.get("filter");
+            for (String filter : filterList) {
+                filter = filter.toLowerCase();
+
+                if (filter.startsWith("author:")) {
+                    filter = filter.substring("author:".length());
+                    for (Book book : filteredBooks) {
+                        if (book.getAuthor().toLowerCase().contains(filter)) {
+                            tempFilteredBooks.add(book);
+                        }
+                    }
+                } else if (filter.startsWith("title:")) {
+                    filter = filter.substring("title:".length());
+                    for (Book book : filteredBooks) {
+                        if (book.getTitle().toLowerCase().contains(filter)) {
+                            tempFilteredBooks.add(book);
+                        }
+                    }
+                } else if (filter.startsWith("available")) {
+                    for (Book book : filteredBooks) {
+                        if (!book.isCheckedOut()) {
+                            tempFilteredBooks.add(book);
+                        }
+                    }
+                }
+
+                filteredBooks = tempFilteredBooks;
+                tempFilteredBooks = new ArrayList<>();
+            }
+        }
+
+
+        if (params.containsKey("sort")) {
+            List<String> sortList = (List<String>) params.get("sort");
+            for (String sort : sortList) {
+                sort = sort.toLowerCase();
+                switch (sort) {
+                    case "isbn" -> filteredBooks.sort(Comparator.comparing(Book::getIsbn));
+                    case "title" -> filteredBooks.sort(Comparator.comparing(Book::getTitle));
+                    case "author" -> filteredBooks.sort(Comparator.comparing(Book::getAuthor));
+                }
+            }
+        }
+
+        return filteredBooks;
     }
 
     // CREATE
