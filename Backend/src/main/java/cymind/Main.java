af9ec487c37e20bc22863800516d23c7fb63a089
Sean Krueger <skrueger2270@gmail.com>
2025-09-20 22:12:06 -0500

    Backend: Remove sample project

---
+++ Backend/src/main/java/cymind/Main.java
@@ -0,0 +1,18 @@
+package cymind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Yaroslav Ziabkin
+ * @author Sean Krueger
+ */
+
+@SpringBootApplication
+class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
