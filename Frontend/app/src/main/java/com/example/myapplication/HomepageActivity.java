5f9e9d6a76fb8d3a654820af5e16dafb2ca8c4bd
Garrett Thompson <gthomp@iastate.edu>
2025-09-21 22:10:03 -0500

    In-Progress: homepage

---
+++ Frontend/app/src/main/java/com/example/myapplication/HomepageActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomepageActivity extends AppCompatActivity {
+
+}
