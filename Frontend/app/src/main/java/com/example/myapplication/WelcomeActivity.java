687e9fc832d392bac33d9e92d8bb6e615eb2d133
Garrett Thompson <gthomp@iastate.edu>
2025-09-21 21:45:57 -0500

    In-Progress: welcome page

--- Frontend/app/src/main/java/com/example/myapplication/WelcomeActivity.java
+++ Frontend/app/src/main/java/com/example/myapplication/WelcomeActivity.java
@@ -1,22 +1,66 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonRegister;
+    private Button buttonUserSignin;
+    private Button buttonGuestSignin;
+    private Button buttonProfessionalSignin;
+    private TextView textViewWelcome;
+    private ImageView imageViewLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome); // links to main activity xml file
 
-        // initialize UI elements
+        setContentView(R.layout.activity_welcome);
 
+        // View initializations
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonRegister = findViewById(R.id.register);
+        buttonUserSignin = findViewById(R.id.userSignin);
+        buttonGuestSignin = findViewById(R.id.guestSignin);
+        buttonProfessionalSignin = findViewById(R.id.professionalSignin);
+        textViewWelcome = findViewById(R.id.textViewWelcome);
+        imageViewLogo = findViewById(R.id.imageViewLogo);
 
-        // Welcome Activity UI elements
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: navigate to registration page when made
+            }
+        });
+
+        buttonUserSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: process signin details and navigate to user homepage when made
+            }
+        });
+
+        buttonGuestSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: navigate to homepage without user abilities
+            }
+        });
+
+        buttonProfessionalSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: navigate to professionals sign in page when made
+            }
+        });
     }
 }
