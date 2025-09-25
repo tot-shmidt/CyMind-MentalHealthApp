a6b4c668cfb0d09f129d36fe5a17ffece8f15820
Yaroslav Ziabkin <yarik@iastate.edu>
2025-09-25 12:09:25 -0500

    Work with http-requests.

---
+++ Experiments/Yaroslav Ziabkin/tutorials-springboot_unit2_1_onetoone/springboot_example/src/main/java/onetoone/Persons/PersonRepository.java
@@ -0,0 +1,20 @@
+package onetoone.Persons;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 
+ * @author Vivek Bengre
+ * 
+ */ 
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    
+    Person findById(int id);
+
+    @Transactional
+    void deleteById(int id);
+
+    Person findByLaptop_Id(int id);
+}
