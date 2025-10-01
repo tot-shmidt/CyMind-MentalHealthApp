67eaa1df4556c0783a00c6bff4844c1af68d3009
Taryn Dunn <tarynad@iastate.edu>
2025-10-01 16:55:15 -0500

    create POST functionality with mock server for new users.

--- Frontend/app/src/main/java/com/example/myapplication/SignUpActivity.java
+++ Frontend/app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -7,7 +7,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -40,14 +53,63 @@ public class SignUpActivity extends AppCompatActivity {
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: navigate to registration page when made
+
             }
         });
 
         buttonUserSignin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: process signin details and navigate to user homepage when made
+                // TODO: navigate to registration page when made
+
+                String userEmail = editTextEmail.getText().toString().trim();
+                String userPass = editTextPassword.getText().toString().trim();
+
+                if(userEmail.isEmpty()) {
+                    editTextEmail.setError("Email field is required");
+                    return;
+                }
+
+                if(userPass.isEmpty()) {
+                    editTextPassword.setError("Password field is required");
+                    return;
+                }
+                JSONObject request = new JSONObject();
+                try {
+                    request.put("email", userEmail);
+                    request.put("password", userPass);
+                }
+                catch(JSONException e) {
+                    e.printStackTrace();
+                    return;
+                }
+
+                String postUrl = "https://f5fb9954-c023-4687-984d-af55d0cd74f2.mock.pstmn.io/users";
+                RequestQueue queue = Volley.newRequestQueue(SignUpActivity.this);
+
+                JsonObjectRequest post = new JsonObjectRequest(Request.Method.POST, postUrl, request,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String postedEmail = response.getString("email");
+                            String postedPass = response.getString("password");
+
+                            Toast.makeText(SignUpActivity.this, "New user with email:" + postedEmail + "Password is: " + postedPass,
+                                    Toast.LENGTH_LONG).show();
+                        } catch(JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError err) {
+                                Toast.makeText(SignUpActivity.this, "Error request: " + err.getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+                queue.add(post);
 
             }
         });
