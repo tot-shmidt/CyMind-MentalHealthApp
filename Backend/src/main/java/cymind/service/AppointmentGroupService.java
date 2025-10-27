e249f5e15a5e91ea98ec22bd47ee54b799ede592
Sean Krueger <skrueger2270@gmail.com>
2025-10-27 01:26:02 -0500

    Backend: Use DTO for Appointment Group

--- Backend/src/main/java/cymind/service/AppointmentGroupService.java
+++ Backend/src/main/java/cymind/service/AppointmentGroupService.java
@@ -19,6 +19,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @Slf4j
