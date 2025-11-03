f5f6afc74bfb42e40fb88291a458264c0b750b8c
Yaroslav Ziabkin <yarik@iastate.edu>
2025-11-02 18:49:07 -0600

    first websocket setup

--- Backend/src/main/java/cymind/Main.java
+++ Backend/src/main/java/cymind/Main.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
-class Main {
+public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
---
+++ Backend/src/main/java/cymind/websocket2/NotificationSocket.java
@@ -0,0 +1,31 @@
+package cymind.websocket2;
+
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnError;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.PathParam;
+import jakarta.websocket.server.ServerEndpoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@ServerEndpoint(value = "/notifications/{userId}")
+public class NotificationSocket {
+
+    // Store all socket session and their corresponding username.
+    private static Map<Session, String> sessionUsernameMap = new Hashtable<>();
+    private static Map<String, Session> usernameSessionMap = new Hashtable<>();
+
+    private final Logger logger = LoggerFactory.getLogger(NotificationSocket.class);
+
+}
