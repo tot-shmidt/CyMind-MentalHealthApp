fa04a2558b3a83ee3ef76f2b3b231074527ae12d
Yaroslav Ziabkin <yarik@iastate.edu>
2025-11-04 07:41:51 -0600

    Fix ArticleDTO to prevent infinite loop

--- Backend/src/main/java/cymind/dto/article/ArticleDTO.java
+++ Backend/src/main/java/cymind/dto/article/ArticleDTO.java
@@ -2,13 +2,22 @@ package cymind.dto.article;
 
 import cymind.dto.user.ProfessionalDTO;
 import cymind.model.Article;
-import cymind.model.MentalHealthProfessional;
-
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public record ArticleDTO(Long id,String articleName, String category1, String category2, String category3,
-                         String content, Set<MentalHealthProfessional> authors) {
+public record ArticleDTO(Long id, String articleName, String category1, String category2, String category3,
+                        String content, Set<ProfessionalDTO> authors) {
     public ArticleDTO(Article article) {
-        this(article.getId(),article.getArticleName(), article.getCategory1(), article.getCategory2(), article.getCategory3(), article.getContent(), article.getAuthors());
+        this(
+            article.getId(),
+            article.getArticleName(),
+            article.getCategory1(),
+            article.getCategory2(),
+            article.getCategory3(),
+            article.getContent(),
+            article.getAuthors().stream()
+                .map(ProfessionalDTO::new)
+                .collect(Collectors.toSet())
+        );
     }
 }
